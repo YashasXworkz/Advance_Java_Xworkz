fb4f81e423a342571083a6cda6c16d40342ed05c
YashasXworkz <yashass.xworkz@gmail.com>
2023-09-14 20:00:52 +0530

    Added Jpa_Hibernate_servlet CRUD Oper java related files

--- passport/src/main/java/com/xworkz/passport/controller/DeleteServlet.java
+++ passport/src/main/java/com/xworkz/passport/controller/DeleteServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.xworkz.passport.entity.PassportEntity;
 import com.xworkz.passport.service.PassportService;
 import com.xworkz.passport.service.PassportServiceImpl;
 
