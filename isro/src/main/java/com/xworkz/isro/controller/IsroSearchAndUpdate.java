725a5165e907edc7d90721cbf361588be4f000b8
YashasXworkz <yashass.xworkz@gmail.com>
2023-10-28 20:30:33 +0530

    Added Placeholders in NamedQuery

--- isro/src/main/java/com/xworkz/isro/controller/IsroSearchAndUpdate.java
+++ isro/src/main/java/com/xworkz/isro/controller/IsroSearchAndUpdate.java
@@ -1,7 +1,6 @@
 package com.xworkz.isro.controller;
 
 import javax.persistence.EntityManager;
-
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
